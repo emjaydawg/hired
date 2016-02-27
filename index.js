a18402d269051887832d0b6327e7c22cbb9f54b7
ian.norris <ian.norris@ip-10-230-113-19.us-west-2.compute.internal>
2016-02-27 13:09:18 -0800

    switching to express js so that jade can be used too

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-var static = require('node-static');
+// var static = require('node-static');
 
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var file = new static.Server('./public');
+// //
+// // Create a node-static server instance to serve the './public' folder
+// //
+// var file = new static.Server('./public');
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT || 5000);
+// require('http').createServer(function (request, response) {
+//     request.addListener('end', function () {
+//         //
+//         // Serve files!
+//         //
+//         file.serve(request, response);
+//     }).resume();
+// }).listen(process.env.PORT || 5000);
+
+
+var express = require('express');
+var app = express();
+var jadeStatic = require('jade-static');
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(jadeStatic(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
