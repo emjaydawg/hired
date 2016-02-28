596e149eadc4f70ad943b36000bf7a3dff8c9216
Pierre Jambet <pierre.jambet@gmail.com>
2016-02-28 14:23:37 -0800

    CSS tweaks

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 				$("#menu-button").click(function(){
 					$("#menu").toggle();
+          var $jobs = $('#jobs');
+          if ($jobs.is(':visible')) {
+            $jobs.hide();
+          } else {
+            $jobs.show();
+          }
 				})
 				$('.heart').hover(
 					function(){
